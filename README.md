3ca09faa8859aa1994b84cd9387a37f27839673a
Sudhin Varghese <sudh_chandy@yahoo.com>
2016-01-23 15:38:23 -0600

    Added the note list to state and included the Notes list in the App

--- README.md
+++ README.md
@@ -2,12 +2,12 @@
 A Kanban App as part of learning webpack and React. Following the instructions from http://survivejs.com/ e-Book
 
 # Setup
-Prerequisite:
-- node.js and npm installed
-- A good editor: my preference Atom with Nuclide
+## Prerequisite:
+- __node.js__ and __npm__ installed
+- A good editor: my preference *Atom with Nuclide*
 
 After cloning, you can run the following commands to get started:
 - `npm install`
 - `npm start`
 
- After this, you can check out the app from your browser - http://localhost:8080/
+ After this, you can check out the app from your browser - [http://localhost:8080/]
--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,39 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    return <Note />;
+
+    const notes = this.state.notes;
+
+    return (
+      <div>
+        <ul>{notes.map(note =>
+          <li key={note.id}>{note.task}</li>
+        )}</ul>
+      </div>
+    );
   }
 }
